876925199381bf95c6f01194587caa8e2885d871
HP <HP@DESKTOP-UFADM6T>
2022-12-17 12:29:22 +0530

    factorial of number

---
+++ bhavishya/Factorialmethod.java
@@ -0,0 +1,15 @@
+package com.factorialproblem;
+
+public class Factorialmethod {
+
+	public static void main(String[] args) {
+		int a = 5;
+		int fact=1;
+		for(int i=1;i<=5;i++)
+		{
+			fact=fact*i;
+    	}
+		System.out.println("Factorial of 5 is :"+fact);
+	}
+
+}
