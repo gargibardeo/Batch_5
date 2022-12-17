75ae47843a046f1469a144c494741b3120917028
anike <anike@LAPTOP-6FH2MD45>
2022-12-17 15:28:51 +0530

    dateAndTime

---
+++ aniket_gaikwad/LocalDateTimeExample.java
@@ -0,0 +1,13 @@
+package com.datetime;
+
+import java.time.LocalDateTime;  
+
+import java.time.format.DateTimeFormatter;  
+public class LocalDateTimeExample {  
+  public static void main(String[] args) {  
+    LocalDateTime datetime = LocalDateTime.now(); 
+   
+      
+    System.out.println(datetime);    
+  }  
+}  
