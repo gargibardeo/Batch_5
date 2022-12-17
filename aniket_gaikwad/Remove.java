27bf16fdff54cc833cd1e75e5e64ef01efad5267
anike <anike@LAPTOP-6FH2MD45>
2022-12-17 15:52:15 +0530

    Remove A

---
+++ aniket_gaikwad/Remove.java
@@ -0,0 +1,10 @@
+package com.String;
+
+public class Remove {
+
+	public static void main (String[] args) {
+		String s = "JAVA IS A SIMPLEST PROGRAMMING LANGUAGE";
+		String replace = s.replace("A","");
+		System.out.println(replace);
+	}
+}
